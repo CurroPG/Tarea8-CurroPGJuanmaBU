3ff17084abafb8ca8b598e40034df96e151f8578
Curro Portillo Guerrero <cporgue2307@g.educaand.es>
2025-10-31 14:14:58 +0100

    segundo commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("TArea 8 de la asignatura de Entornos de desarrollo");
     }
 }
