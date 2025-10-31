e4abae51170c2cd13ab98e6cf417fc5e5bcf127e
Juan Manuel Baena <jbaeurb3009@g.educaand.es>
2025-10-31 14:27:25 +0100

    Modificacion de codigo java y ampliacion de codigo de juanma

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("TArea 8 de la asignatura de Entornos de desarrollo");
+        System.out.println("Tarea 8 de la asignatura de Entornos de desarrollo");
+	System.out.println("Hola soy juanma y esta es la modificacion");
     }
 }
